a99713cb105d1c5cd237a0dab7bc114af21ec3b2
max-denisov <max.denisov@inbox.ru>
2016-10-23 22:09:06 +0200

    some changes in work
    Created class Game. Added mirror image to moving. Reworked infinity loop.

--- Classes.py
+++ Classes.py
@@ -4,18 +4,40 @@ CREATURE_COORD = CREATURE_COORD_X, CREATURE_COORD_Y = 200, 100
 HERO_BASE_COORD = HERO_COORD_X, HERO_COORD_Y = 300, 100
 CREATURE_HOME = FIRST_HOME_COORD, SECOND_HOME_COORD = [0, 0], [300, 200]
 SCREEN_SIZE = WIDTH, HEIGHT = 800, 600
+BASE_SCREEN_BG = [255, 255, 255]
 
 # hp, speed, danger level, damage, armor
 bestiary = {
     'dummy': [5, 2, 0, 0, 0],
-    'base_hero': [13, 5, 2, 4, 1]
+    'lil_hero': [7, 6, 1, 2, 2]
 }
 
-# forward, (left, backward, right)
+# left, right, (forward, backward)
 images = {
-    'dummy': ['dummy.png'],
-    'base_hero': ['base_hero.png']
+    'dummy': ['dummy_left.png', 'dummy_right.png'],
+    'lil_hero': ['lil_hero_left.png', 'lil_hero_right.png']
 }
+
+arrows = {
+    'up': pygame.K_UP,
+    'down': pygame.K_DOWN,
+    'left': pygame.K_LEFT,
+    'right': pygame.K_RIGHT
+}
+
+
+def press_arrows(event, hero, pressed_arrows):
+    for direction in arrows:
+        if event.key == arrows[direction]:
+            hero.state_edit(direction)
+            pressed_arrows.append(direction)
+
+
+def release_arrows(event, hero, pressed_arrows):
+    for direction in arrows:
+        if event.key == arrows[direction]:
+            pressed_arrows.remove(direction)
+            hero.state_edit(pressed_arrows[len(pressed_arrows) - 1])
 
 
 def base_move_correction(rect, screen=SCREEN_SIZE):
@@ -30,6 +52,28 @@ def base_move_correction(rect, screen=SCREEN_SIZE):
     return rect
 
 
+class Game(object):
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(SCREEN_SIZE)
+        self.pressed_arrows = ['still']
+
+    def infinity_loop(self, hero):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                press_arrows(event, hero, self.pressed_arrows)
+            elif event.type == pygame.KEYUP:
+                release_arrows(event, hero, self.pressed_arrows)
+        hero.moving()
+        self.screen.fill(BASE_SCREEN_BG)
+        self.screen.blit(hero.current_image, hero.rect)
+        pygame.display.flip()
+        pygame.display.update()
+        pygame.time.wait(80)
+
+
 class Creature:
     def __init__(self, creature='dummy', coordinates=CREATURE_COORD,
                  # move it somewhere else:
@@ -41,14 +85,20 @@ class Creature:
         self.danger_level = bestiary[creature][2]
         self.attack_strength = bestiary[creature][3]
         self.damage_resist = bestiary[creature][4]
-        self.forward_image = pygame.image.load(images[creature][0])
-        self.forward_rect = self.forward_image.get_rect()
+        self.image = {
+            'left': pygame.image.load(images[creature][0]),
+            'right': pygame.image.load(images[creature][1])
+        }
+        self.current_image = self.image['left']
+        self.rect = self.image['left'].get_rect()
         self.state = 'still'
         if coordinates[0] < 0 or coordinates[1] < 0:
             print 'In class Creature __init__():'
             print 'Negative coordinates of creature do not have to be used.', coordinates
             self.coordinates = [CREATURE_COORD_X, CREATURE_COORD_Y]
             print 'Coordinates are', self.coordinates
+        else:
+            self.coordinates = coordinates
         self.direction = direction
         # move it somewhere else:
         # if home_location[0][0] < 0 or home_location[0][1] < 0 or home_location[1][0] < 0 or home_location[1][1] < 0:
@@ -69,11 +119,12 @@ class Creature:
 
 
 class MainHero(Creature):
-    def __init__(self, creature='base_hero', coordinates=HERO_BASE_COORD, direction='forward'):
+
+    def __init__(self, creature='lil_hero', coordinates=HERO_BASE_COORD, direction='forward'):
         Creature.__init__(self, creature=creature, coordinates=coordinates, direction=direction)
 
     def state_edit(self, direction='not_chosen'):
-        if direction in ('left', 'right', 'up', 'down'):
+        if direction in ('left', 'right', 'up', 'down', 'still'):
             self.state = direction
         else:
             print 'In class Creature state_edit():'
@@ -85,8 +136,10 @@ class MainHero(Creature):
         move_shift = [0, 0]
         if self.state == 'right':
             move_shift = [self.move_speed, 0]
+            self.current_image = self.image['right']
         elif self.state == 'left':
             move_shift = [-self.move_speed, 0]
+            self.current_image = self.image['left']
         elif self.state == 'down':
             move_shift = [0, self.move_speed]
         elif self.state == 'up':
@@ -94,54 +147,16 @@ class MainHero(Creature):
         elif self.state == 'still':
             move_shift = [0, 0]
         if move_shift != [0, 0]:
-            self.forward_rect = self.forward_rect.move(move_shift)
-            self.forward_rect = base_move_correction(self.forward_rect)
+            self.rect = self.rect.move(move_shift)
+            self.rect = base_move_correction(self.rect)
 
-    def __del__(self):
-        # here's just nothing to do now
-        pass
 
 def main():
-        pygame.init()
-        test = MainHero(creature='base_hero', coordinates=[300, 200])
-        screen = pygame.display.set_mode(SCREEN_SIZE)
-        pressed_arrows = ['still']
+        just_world = Game()
+        hero = MainHero(creature='lil_hero', coordinates=[300, 200])
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        test.state_edit('up')
-                        pressed_arrows.append('up')
-                    if event.key == pygame.K_DOWN:
-                        test.state_edit('down')
-                        pressed_arrows.append('down')
-                    if event.key == pygame.K_LEFT:
-                        test.state_edit('left')
-                        pressed_arrows.append('left')
-                    if event.key == pygame.K_RIGHT:
-                        test.state_edit('right')
-                        pressed_arrows.append('right')
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
-                        pressed_arrows.remove('up')
-                        test.state_edit(pressed_arrows[len(pressed_arrows) - 1])
-                    if event.key == pygame.K_DOWN:
-                        pressed_arrows.remove('down')
-                        test.state_edit(pressed_arrows[len(pressed_arrows) - 1])
-                    if event.key == pygame.K_LEFT:
-                        pressed_arrows.remove('left')
-                        test.state_edit(pressed_arrows[len(pressed_arrows) - 1])
-                    if event.key == pygame.K_RIGHT:
-                        pressed_arrows.remove('right')
-                        test.state_edit(pressed_arrows[len(pressed_arrows) - 1])
-            test.moving()
-            screen.fill([0, 0, 0])
-            screen.blit(test.forward_image, test.forward_rect)
-            pygame.display.flip()
-            pygame.display.update()
-            pygame.time.wait(40)
+            just_world.infinity_loop(hero)
+
 
 try:
     main()
