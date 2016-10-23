7cc77ee3e33b066c8cf9bd64f085bd377f883595
Aryslan Yakshibaev <yak.aryslan.1999@gmail.com>
2016-10-23 23:37:18 +0300

    Added class Item

--- Classes.py
+++ Classes.py
@@ -5,12 +5,17 @@ CREATURE_COORD_Y = 100
 
 # hp, speed, danger level, damage, armor
 bestiary = {
-    'dummy': [5, 0, 0, 0, 0]
+    'dummy': [5, 0, 0, 0, 0],
+}
+
+#
+items = {
+    0: {"name": "Nothing", "icon": None, "weight": 0, "cost": 0, "description": "Empty", "item_id": 0},
 }
 
 # forward, (left, backward, right)
 images = {
-    'dummy': ['dummy.png']
+    'dummy': ['dummy.png'],
 }
 
 
@@ -47,6 +52,83 @@ class Creature:
         pass
 
 
+class Item:
+    __free_item_id__ = 1
+    
+    def __getNewItemId__(self):
+        Item.__free_item_id__ += 1
+        return Item.__free_item_id__ - 1
+    
+    def __init__(self, identifier, coords):  # int identifier, (int, int) coords.
+        if identifier not in items.keys():
+            raise ValueError('Id ' + str(identifier) + 'not found!')
+        self.info = items[int(identifier)]
+        self.on_ground = True
+        self._id_ = identifier
+        self.item_id = self.__getNewItemId__()
+        self.x = coords[0]
+        self.y = coords[1]
+        # del self.getNewItemId  # this function shouldn't be in this class's objects.
+
+    def getItemId(self):
+        return self._id_
+
+    def getId(self):
+        return self.item_id
+
+    def isOnGround(self):
+        return self.on_ground
+
+    def getIcon(self):
+        return self.info["icon"]
+
+    def getCoords(self):
+        # If item is in inventory, func returns inventory coords. Else it returns world-related coords.
+        return {"on ground": self.isOnGround(), "x": self.x, "y": self.y}
+
+    def getWeight(self):
+        return self.info["weight"]
+
+    def getCost(self):
+        return self.info["cost"]
+
+    def getDescription(self):
+        return self.info["description"]
+
+    def getTimeLeft(self):
+        if True:
+            raise NotImplementedError('Class Item, method getTimeLeft. Method moveToGround is required.')
+        if self.on_ground:
+            if self.start + self.duration <= game_time:  # Time must be global.
+                pass  # Here should be deletion of item.
+            return self.start + self.duration - game_time
+        return None
+    
+    def getInfo(self):
+        # This func isn't cool, it can work longer than other.
+        info = self.info
+        info["on_ground"] = self.isOnGround()
+        info["id"] = self.getId()
+        info["coords"] = (self.x, self.y)
+        return info
+
+    def moveToInventory(self):
+        # Here should be choice of coord in player' inventory.
+        # Will be realized after realization of Player class.
+        raise NotImplementedError('Class Item, method moveToInventory')
+
+    def moveToGround(self):
+        # Here should be choice of coords based on player' coords and start of item's duration timer.
+        # Will be realized after realization of Player class.
+        raise NotImplementedError('Class Item, method moveToGround')
+
+    def moveInInventory(self, x, y):
+        # Here should be coords check and changing coords of item in Player's inventory too.
+        if True:
+            raise NotImplementedError('Class Item, method moveInInventory')
+        self.coords = (self.coords[0] + x, self.coords[1] + y)
+
+
 def init_screen(height=600, width=800):
     pygame.init()
     size = [width, height]
@@ -56,14 +138,17 @@ def init_screen(height=600, width=800):
 def main():
         test = Creature(coordinates=[200, -300])
         screen = init_screen()
+        game_time = 0  # Global game time from start (in seconds).
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit()
             screen.fill([0, 0, 0])
             screen.blit(test.forward_image, test.coordinates)
+            game_time = game_time + 1
             pygame.display.update()
             pygame.time.delay(100)
+            
 
 try:
     main()
